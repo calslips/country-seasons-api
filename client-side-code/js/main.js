fb743d424c2499ecc812326671b5d479553904c1
Caleb Phillips <csphillips5@gmail.com>
2022-06-07 16:20:26 -0400

    add demo client side code

---
+++ client-side-code/js/main.js
@@ -0,0 +1,40 @@
+document.querySelector('button').addEventListener('click', getCountry);
+
+async function getCountry() {
+  const country = document.querySelector('input').value
+
+  let div = document.querySelector('div');
+  if (div) {
+    document.body.removeChild(div);
+  }
+  div = document.createElement('div');
+  document.body.appendChild(div);
+  // if (document.querySelector('div')) {
+  //   document.body.removeChild('div');
+  //  } else {
+  //    let div = document.createElement('div');
+  //  }
+  // document.body.appendChild(div);
+  try {
+    const response = await fetch(`https://country-seasons-api.herokuapp.com/api/${country}`);
+    const data = await response.json();
+    console.log(data);
+    const h2 = document.createElement('h2');
+    h2.textContent = `${
+      country.split(' ').map(s => s[0].toUpperCase() + s.slice(1).toLowerCase()).join(' ')
+    }`;
+    div.appendChild(h2);
+    for (let season in data.seasons) {
+      console.log(season);
+      const temps = data.seasons[season].fahrenheitTemps;
+      const h3 = document.createElement('h3')
+      h3.textContent = `${season[0].toUpperCase() + season.slice(1)}`;
+      div.appendChild(h3);
+      const p = document.createElement('p');
+      p.textContent = `Average Temp Range: ${temps.minAverageRange[0]} - ${temps.maxAverageRange[1]} F`;
+      div.appendChild(p);
+    }
+  } catch(err) {
+    console.error(err);
+  }
+}
